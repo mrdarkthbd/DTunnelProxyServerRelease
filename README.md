2fe2884e93f0cac7006fa4f0acfb845fd81a5e6a
MrDark <116535388+mrdarkthbd@users.noreply.github.com>
2024-02-24 11:45:35 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# DTunnelProxyServerRelease
+# DT Proxy Installation
+
+To install DT Proxy, run the following command in your terminal:
+
+```
+curl -s -L https://raw.githubusercontent.com/mrdarkthbd/DTunnelProxyServerRelease/main/install.sh
+```
+# OU
+
+````
+bash <(curl -sL https://raw.githubusercontent.com/mrdarkthbd/DTunnelProxyServerRelease/main/install.sh)
+````
---
+++ main.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+
+TOKEN_FILE="$HOME/.proxy_token"
+PROXY_BIN="/usr/bin/proxy"
+
+prompt() {
+    echo -e "\033[1;33m$1\033[0m"
+}
+
+is_port_in_use() {
+    local port=$1
+    nc -z localhost "$port"
+    return $?
+}
+
+show_ports_in_use() {
+    local ports_in_use=$(systemctl list-units --all --plain --no-legend | grep -oE 'proxy-[0-9]+' | cut -d'-' -f2)
+    if [ -n "$ports_in_use" ]; then
+        ports_in_use=$(echo "$ports_in_use" | tr '\n' ' ')
+        echo -e "\033[1;34m║\033[1;32mEm uso:\033[1;33m $(printf '%-21s' "$ports_in_use")\033[1;34m║\033[0m"
+        echo -e "\033[1;34m║═════════════════════════════║\033[0m"
+    fi
+}
+
+get_yes_no_response() {
+    local response question="$1" 
+    while true; do
+        read -rp "$question (s/n): " -ei n response
+        case "$response" in
+            [sS]) return 0 ;;
+            [nN]) return 1 ;;
+            *) echo -e "\033[1;31mInvalid response. Try again.\033[0m" ;;
+        esac
+    done
+}
+
+pause_prompt() {
+    read -rp "$(prompt 'Enter to continue...')" voidResponse
+}
+
+get_valid_port() {
+    while true; do
+        read -rp "$(prompt 'Porta: ')" port
+        if ! [[ "$port" =~ ^[0-9]+$ ]]; then
+            echo -e "\033[1;31mInvalid Port.\033[0m"
+        elif [ "$port" -le 0 ] || [ "$port" -gt 65535 ]; then
+            echo -e "\033[1;31mPort out of allowed range.\033[0m"
+        elif is_port_in_use "$port"; then
+            echo -e "\033[1;31mPort in use.\033[0m"
+        else
+            break
+        fi
+    done
+    echo "$port"
+}
+
+start_proxy() {
+    local port=$(get_valid_port)
+    local protocol cert_path response ssh_only service_name service_file
+    local proxy_log_file="/var/log/proxy-$port.log"
+
+    if get_yes_no_response "$(prompt 'Habilitar o HTTPS?')"; then
+        protocol="--https"
+        read -rp "$(prompt 'Certificate SSL (HTTPS):')" cert_path
+        cert_path="--cert $cert_path"
+    else
+        protocol="--http"
+        cert_path=""
+    fi
+
+    read -rp "$(prompt 'Status HTTP (Standard: @DuTra01): ')" response
+    response="${response:-@DuTra01}"
+
+    if get_yes_no_response "$(prompt 'Enable only SSH?')"; then
+        ssh_only="--ssh-only"
+    else
+        ssh_only=""
+    fi
+
+    service_name="proxy-$port"
+    service_file="/etc/systemd/system/$service_name.service"
+    cat > $service_file <<EOF
+[Unit]
+Description=DTunnel Proxy Server on port $port
+After=network.target
+
+[Service]
+Type=simple
+User=$(whoami)
+WorkingDirectory=$(pwd)
+ExecStart=$PROXY_BIN --token $(cat "$TOKEN_FILE") $protocol --port $port $ssh_only --buffer-size 32768 --workers 2500 $cert_path --response $response --log-file $proxy_log_file
+StandardOutput=null
+StandardOutput=null
+Restart=always
+TasksMax=5000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl start "$service_name"
+    systemctl enable "$service_name"
+
+    echo -e "\033[1;32mProxy started at the port $port.\033[0m"
+    pause_prompt
+}
+
+restart_proxy() {
+    local port
+    read -rp "$(prompt 'Porta: ')" port
+
+    local service_name="proxy-$port"
+    if ! systemctl is-active "$service_name" >/dev/null; then
+        echo -e "\033[1;31mProxy on the port $port não está ativo.\033[0m"
+        pause_prompt
+        return
+    fi
+
+    systemctl restart "$service_name"
+
+    echo -e "\033[1;32mProxy on the port $port restarted.\033[0m"
+    pause_prompt
+}
+
+stop_proxy() {
+    show_ports_in_use
+
+    local port
+    read -rp "$(prompt 'Porta: ')" port
+    local service_name="proxy-$port"
+
+    systemctl stop "$service_name"
+    systemctl disable "$service_name"
+    systemctl daemon-reload
+    rm "/etc/systemd/system/$service_name.service"
+
+    echo -e "\033[1;32mProxy on the port $port foi fechado.\033[0m"
+    pause_prompt
+}
+
+show_proxy_log() {
+    local port proxy_log_file
+
+    read -rp "$(prompt 'Porta: ')" port
+    proxy_log_file="/var/log/proxy-$port.log"
+
+    if [[ ! -f $proxy_log_file ]]; then
+        echo -e "\033[1;31mLog file not found\033[0m"
+        pause_prompt
+        return
+    fi
+
+    trap 'break' INT
+
+    while :; do
+        clear
+        cat "$proxy_log_file"
+
+        echo -e "\nPressure \033[1;33mCtrl+C\033[0m return to the menu."
+        sleep 1
+    done
+
+    trap - INT
+}
+
+exit_proxy_menu() {
+    echo -e "\033[1;31mLeaving...\033[0m"
+    exit 0
+}
+
+main() {
+    clear
+
+    echo -e "\033[1;34m╔═════════════════════════════╗\033[0m"
+    echo -e "\033[1;34m║\033[1;41m\033[1;32m      DT  Proxy Menu     \033[0m\033[1;34m║"
+    echo -e "\033[1;34m║═════════════════════════════║\033[0m"
+
+    show_ports_in_use
+
+    local option
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m01\033[1;36m] \033[1;32m• \033[1;31mOPEN PORT           \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m02\033[1;36m] \033[1;32m• \033[1;31mCLOSE PORT          \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m03\033[1;36m] \033[1;32m• \033[1;31mRESTART PORT       \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m04\033[1;36m] \033[1;32m• \033[1;31mMONITOR               \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m00\033[1;36m] \033[1;32m• \033[1;31mEXIT                  \033[1;34m║"
+    echo -e "\033[1;34m╚═════════════════════════════╝\033[0m"
+    read -rp "$(prompt 'Choose an option: ')" option
+
+    case "$option" in
+        1) start_proxy ;;
+        2) stop_proxy ;;
+        3) restart_proxy ;;
+        4) show_proxy_log ;;
+        0) exit_proxy_menu ;;
+        *) echo -e "\033[1;31mInvalid option. Try again.\033[0m" ; pause_prompt ;;
+    esac
+
+    main
+}
+
+main
